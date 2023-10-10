35502143a4adf601ddc02fb5f97fd2ec1358773d
Victor <victorbalducci1@gmail.com>
2023-10-10 17:11:59 +0200

    add popup component

---
+++ frontend/src/components/PopUp.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import styled from "styled-components";
+
+interface PopUpProps {
+  isOpen: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
+}
+
+export const PopUp: React.FC<PopUpProps> = ({ isOpen, onClose, children }) => {
+  if (!isOpen) return null;
+
+  return (
+    <ContainerModal>
+      <Modal>
+        <button onClick={onClose}>Close</button>
+        {children}
+      </Modal>
+    </ContainerModal>
+  );
+};
+
+const ContainerModal = styled.div`
+  position: 'fixed';
+  top: 0;
+  left: 0;
+  width: '100vw';
+  height: '100vh';
+  background-color: 'rgba(0, 0, 0, 0.7)';
+  display: 'flex';
+  align-items: 'center';
+  justify-content: 'center';
+`;
+
+const Modal = styled.div`
+  background-color: 'white';
+  padding: '20px';
+  border-radius: '8px';
+`;
