cd1c63a568c188356e3c4d4db7a0594660a7b4be
Victor <victorbalducci1@gmail.com>
2023-10-12 22:47:41 +0200

    change input in form

--- frontend/src/pages/dashboard/home/components/formCreateUser.tsx
+++ frontend/src/pages/dashboard/home/components/formCreateUser.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { UserFormData } from "../../../../types/usertypes";
 import { FormularInput } from "../../../auth/components/molecules/FormularInput";
 import { Button } from "../../../../components/button";
+import styled from "styled-components";
+import { TitleH2 } from "../../../../components/Title";
   
 export const CreateUserForm: React.FC<{ onSubmit: (data: UserFormData) => void }> = ({ onSubmit }) => {
     const [formData, setFormData] = useState<UserFormData>({
@@ -25,17 +27,52 @@ export const CreateUserForm: React.FC<{ onSubmit: (data: UserFormData) => void }
     };
   
     return (
-        <form onSubmit={handleSubmit}>
-            <input name="firstName" value={formData.firstName} onChange={handleUserFormChange} placeholder="First Name" />
-            <input name="lastName" value={formData.lastName} onChange={handleUserFormChange} placeholder="Last Name" />
-            <input name="email" type="email" value={formData.email} onChange={handleUserFormChange} placeholder="Email" />
-            <input name="password" type="password" value={formData.password} onChange={handleUserFormChange} placeholder="Password" />
-            <input name="companyId" value={formData.companyId} onChange={handleUserFormChange} placeholder="Company ID" />
-            <input name="groupId" value={formData.groupId} onChange={handleUserFormChange} placeholder="Group ID" />
-            <input name="role" value={formData.role} onChange={handleUserFormChange} placeholder="Role" />
-
-            <Button type="submit">Submit</Button>
-        </form>
+        <WrapperForm>
+            <TitleH2>Creation d'utilisateur</TitleH2>
+            <form onSubmit={handleSubmit}>
+                <label>First Name</label>
+                <input name="firstName" value={formData.firstName} onChange={(e) => handleUserFormChange(e.target.name, e.target.value)} placeholder="First Name" />
+                <label>Last Name</label>
+                <input name="lastName" value={formData.lastName} onChange={(e) => handleUserFormChange(e.target.name, e.target.value)} placeholder="Last Name" />
+                <label>Email</label>
+                <input name="email" type="email" value={formData.email} onChange={(e) => handleUserFormChange(e.target.name, e.target.value)} placeholder="Email" />
+                <label>Password</label>
+                <input name="password" type="password" value={formData.password} onChange={(e) => handleUserFormChange(e.target.name, e.target.value)} placeholder="Password" />
+                <label>CompagnyId</label>
+                <input name="companyId" value={formData.companyId} onChange={(e) => handleUserFormChange(e.target.name, e.target.value)} placeholder="Company ID" />
+                <label>GroupeId</label>
+                <input name="groupId" value={formData.groupId} onChange={(e) => handleUserFormChange(e.target.name, e.target.value)} placeholder="Group ID" />
+                <label>Role</label>
+                <select 
+                    name="role" 
+                    value={formData.role} 
+                    onChange={(e) => handleUserFormChange(e.target.name, e.target.value)}
+                >
+                    <option value="ADMIN">ADMIN</option>
+                    <option value="USER">USER</option>
+                </select>
+                <Button type="submit">Submit</Button>
+            </form>
+        </WrapperForm>
     );
 }
  
+const WrapperForm = styled.div` 
+    padding: 20px;
+    width: 500px;
+    form{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        input{
+            margin: 5px 0px;
+        }
+        select{
+            margin: 5px 0px;
+        }
+    }
+`
+
+const InputForm = styled.div`
+    
+`
