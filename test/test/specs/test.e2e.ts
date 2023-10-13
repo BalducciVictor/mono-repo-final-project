25679bb92e270a056bd297d86d53957d4dc0ca92
carloberni <carlo.berni@hetic.net>
2023-10-13 19:32:42 +0200

    add test

--- test/test/specs/test.e2e.ts
+++ test/test/specs/test.e2e.ts
@@ -1,13 +1,38 @@
-import { expect, browser, $$ } from '@wdio/globals'
+import { expect, browser, $$ } from '@wdio/globals';
+import axios from 'axios';
+import AllureReporter from '@wdio/allure-reporter';
 
-describe('My Login application', () => {
+const baseURL = 'http://onby-front.ghgmbbhubqhfbnc2.francecentral.azurecontainer.io/';
+
+describe('Login page test', () => {
+    beforeEach(async () => {
+        await browser.url(baseURL);
+    });
+
     it('should have correct h1 text', async () => {
-        await browser.url(`http://onby-front.ghgmbbhubqhfbnc2.francecentral.azurecontainer.io/`)
+        const headers = await $$('h1');
+        
+        AllureReporter.addStep('Vérifier que au moins un élément h1 est présent');
+        expect(headers).toBeElementsArrayOfSize({ gte: 1 });
 
-        const headers = await $$('h1')  // Sélection de tous les éléments h1
-        expect(headers).toBeElementsArrayOfSize({ gte: 1 })  // Vérifier qu'au moins un h1 existe
+        AllureReporter.addStep('Vérifier que le texte de h1 contient "Welcome"');
+        const headerText = await headers[0].getText();
+        expect(headerText).toContain('Welcome');
 
-        // Vous pouvez également vérifier le texte du premier h1 si vous le souhaitez
-        expect(headers[0]).toHaveTextContaining('Expected Text Here')
-    })
-})
+        if (headerText !== 'Welcome') {
+            AllureReporter.addStep('Échec : Le texte du h1 n’est pas "Welcome"');
+            AllureReporter.addAttachment('Échec détail', 'Le texte du h1 n’est pas "Welcome"', 'text/plain');
+        }
+    });
+
+    it('should return 200', async () => {
+        AllureReporter.addStep('Vérifier que le code de statut de la réponse est 200');
+        const response = await axios.get(baseURL);
+        expect(response.status).toBe(200);
+
+        if (response.status !== 200) {
+            AllureReporter.addStep('Échec : Le code de statut n’est pas 200');
+            AllureReporter.addAttachment('Échec détail', `Le code de statut est ${response.status}`, 'text/plain');
+        }
+    });
+});
