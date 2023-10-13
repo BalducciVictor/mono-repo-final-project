fcb6dc2d6457e1496298557d63a854fcdbb7510f
carloberni <carlo.berni@hetic.net>
2023-10-13 20:41:57 +0200

    does not work when it's false

--- test/wdio.conf.ts
+++ test/wdio.conf.ts
@@ -61,7 +61,10 @@ export const config: Options.Testrunner = {
     // https://saucelabs.com/platform/platform-configurator
     //
     capabilities: [{
-        browserName: 'chrome'
+        browserName: 'chrome',
+        'goog:chromeOptions': {
+            args: ['headless', 'disable-gpu']
+        }
     }],
 
     //
@@ -111,7 +114,7 @@ export const config: Options.Testrunner = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    // services: [],
+     services: ['chromedriver'],
     //
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
