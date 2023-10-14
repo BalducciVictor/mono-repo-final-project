25fc23fd909a4f2d3717cdc1a53d545f8d8ca6fe
carloberni <carlo.berni@hetic.net>
2023-10-14 18:40:45 +0200

    finish loginpage

--- test/test/specs/loginPage.spec.ts
+++ test/test/specs/loginPage.spec.ts
@@ -7,32 +7,61 @@ const baseURL = 'http://onby-front.ghgmbbhubqhfbnc2.francecentral.azurecontainer
 describe('Login page test', () => {
     before(async () => {
         await browser.url(baseURL);
-    })
+    });
     
     it('should return 200', async () => {
         AllureReporter.startStep('Vérifier que le code de statut de la réponse est 200');
-        const response = await axios.get(baseURL);
-        AllureReporter.addAttachment('Statut de réponse reçu', `${response.status}`, 'text/plain');
-        expect(response.status).toBe(200);
-        AllureReporter.endStep();
-    })
+        try {
+            const response = await axios.get(baseURL);
+            AllureReporter.addAttachment('Statut de réponse reçu', `${response.status}`, 'text/plain');
+            expect(response.status).toBe(200);
+        } catch (error: unknown) {
+            if (error instanceof Error) {
+                AllureReporter.addAttachment('Erreur', error.toString(), 'text/plain');
+                throw error;
+            } else {
+                throw new Error('le code de la page ne correspond pas');
+            }
+        } finally {
+            AllureReporter.endStep();
+        }
+    });
         
     it("should have exactly one H1 displayed", async () => {
         AllureReporter.startStep('Vérifier qu’un seul élément h1 est présent');
-        const headers = await $$("h1");
-        AllureReporter.addAttachment('Nombre d\'éléments h1 trouvés', `${headers.length}`, 'text/plain');
-        expect(headers.length).toBe(1);
-        AllureReporter.endStep();
-    })
+        try {
+            const headers = await $$("h1");
+            AllureReporter.addAttachment('Nombre d\'éléments h1 trouvés', `${headers.length}`, 'text/plain');
+            expect(headers.length).toBe(1);
+        } catch (error: unknown) {
+            if (error instanceof Error) {
+                AllureReporter.addAttachment('Erreur', error.toString(), 'text/plain');
+                throw error;
+            } else {
+                throw new Error('erreur, il y a plus de 1 h1');
+            }
+        } finally {
+            AllureReporter.endStep();
+        }
+    });
         
     it('should have correct h1 text', async () => {
         AllureReporter.startStep('Vérifier que le texte de h1 contient "Welcome Back!"');
-        const headers = await $$("h1");
-        const headerText = await headers[0].getText();
-        AllureReporter.addAttachment('Texte h1 reçu', `${headerText}`, 'text/plain');
-        console.log(headerText)
-        expect(headerText).toEqual("Welcome Back!"); 
-        AllureReporter.endStep();  
-    })
+        try {
+            const headers = await $$("h1");
+            const headerText = await headers[0].getText();
+            AllureReporter.addAttachment('Texte h1 reçu', `${headerText}`, 'text/plain');
+            expect(headerText).toEqual("Welcome Back!"); 
+        } catch (error: unknown) {
+            if (error instanceof Error) {
+                AllureReporter.addAttachment('Erreur', error.toString(), 'text/plain');
+                throw error;
+            } else {
+                throw new Error('le h1 ne correspond pas');
+            }
+        } finally {
+            AllureReporter.endStep();  
+        }
+    });
     
 });
