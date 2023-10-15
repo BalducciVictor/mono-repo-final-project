d59917be3fb8f1e610158cc22a898826c91a0e76
MaksymY <maksym.yankivskyypro@gmail.com>
2023-10-15 12:42:16 +0200

    add style for company and fix chapters

--- frontend/src/pages/dashboard/home/components/molecules/CompanyCard.tsx
+++ frontend/src/pages/dashboard/home/components/molecules/CompanyCard.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import IllustrationCompany from '../../../../../assets/company.png';
 
 type CompanyCardProps = {
     CompanyName: string;
@@ -11,9 +12,12 @@ type CompanyCardProps = {
 export const CompanyCard = ({ CompanyName, HandleDelete, HandleModifier, CompanyId, companyData }: CompanyCardProps) => {
     return (
         <Company>
-            <p>{CompanyName}</p>
-            <button onClick={() => HandleDelete(CompanyId)}>Supprimer</button>
-            <button onClick={() => HandleModifier(companyData)}>Modifier</button>
+            <img src={IllustrationCompany} alt="" />
+            <div>
+                <p>{CompanyName}</p>
+                <DeleteButton onClick={() => HandleDelete(CompanyId)}>Supprimer</DeleteButton>
+                <ModifierButton onClick={() => HandleModifier(companyData)}>Modifier</ModifierButton>
+            </div>
         </Company>
     )
 }
@@ -24,4 +28,52 @@ const Company = styled.li`
     box-shadow: 2px 0px 20px 0px rgba(0, 0, 0, 0.10);
     padding: 20px;
     width: 300px;
+    display: flex;
+    gap: 10px;
+    align-items: center;
+
+    img {
+        width: 50px
+    }
+
+    p {
+        color: #121212;
+        font-size: 16px;
+        font-style: normal;
+        font-weight: 500;
+        max-width: 100%;
+        margin-bottom: 5px;
+        text-overflow: ellipsis;
+        overflow: hidden;
+        white-space: nowrap;
+    }
+`;
+
+const DeleteButton = styled.button`
+    background-color: #f44336;
+    color: white;
+    border: none;
+    padding: 5px 10px;
+    cursor: pointer;
+    border-radius: 5px;
+    transition: background-color 0.3s;
+    margin-right: 10px;
+
+    &:hover {
+        background-color: #e53935;
+    }
+`;
+
+const ModifierButton = styled.button`
+    background-color: #31B898;;
+    color: white;
+    border: none;
+    padding: 5px 10px;
+    cursor: pointer;
+    border-radius: 5px;
+    transition: background-color 0.3s;
+
+    &:hover {
+        background-color: #23866f;;
+    }
 `;
